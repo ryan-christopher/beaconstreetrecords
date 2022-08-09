5ea663d18b0c68ec8385c0f247a96e071d212fa8
ryan-christopher <rechristopher99@gmail.com>
2022-08-08 23:12:15 -0400

    changed animejs link to cdn

--- scripts/soundwave.js
+++ scripts/soundwave.js
@@ -1,20 +1,21 @@
+
 function fitElementToParent(el, padding) {
   var timeout = null;
   function resize() {
     if (timeout) clearTimeout(timeout);
-    anime.set(el, {scale: 1});
+    anime.set(el, { scale: 1 });
     var pad = padding || 0;
     var parentEl = el.parentNode;
     var elOffsetWidth = el.offsetWidth - pad;
     var parentOffsetWidth = parentEl.offsetWidth;
     var ratio = parentOffsetWidth / elOffsetWidth;
-    timeout = setTimeout(anime.set(el, {scale: ratio}), 10);
+    timeout = setTimeout(anime.set(el, { scale: ratio }), 10);
   }
   resize();
   window.addEventListener('resize', resize);
 }
 
-var easingsAnimation = (function() {
+var easingsAnimation = (function () {
 
   var easingVisualizerEl = document.querySelector('.easing-visualizer');
   var barsWrapperEl = easingVisualizerEl.querySelector('.bars-wrapper');
@@ -24,7 +25,7 @@ var easingsAnimation = (function() {
   var numberOfBars = 91;
   var duration = 450;
   var animation;
-  
+
   fitElementToParent(easingVisualizerEl, 0);
 
   for (var i = 0; i < numberOfBars; i++) {
@@ -39,13 +40,13 @@ var easingsAnimation = (function() {
 
   barsWrapperEl.appendChild(barsFragment);
   dotsWrapperEl.appendChild(dotsFragment);
-  
+
   function play() {
-    
+
     var easings = [];
     for (let ease in anime.penner) easings.push(ease);
-    easings.push('steps('+anime.random(5, 20)+')');
-    easings.push('steps('+anime.random(5, 20)+')');
+    easings.push('steps(' + anime.random(5, 20) + ')');
+    easings.push('steps(' + anime.random(5, 20) + ')');
     easings.push('cubicBezier(0.545, 0.475, 0.145, 1)');
     var ease = easings[anime.random(0, easings.length - 1)];
 
@@ -54,19 +55,19 @@ var easingsAnimation = (function() {
       easing: ease,
       complete: play
     })
-    .add({
-      targets: '.easing-visualizer .bar',
-      scaleY: anime.stagger([1, 44], {easing: ease, from: 'center', direction: 'reverse'}),
-      delay: anime.stagger(7, {from: 'center'})
-    })
-    .add({
-      targets: '.easing-visualizer .dot',
-      translateY: anime.stagger(['-160px', '160px'], {easing: ease, from: 'last'}),
-      delay: anime.stagger(7, {from: 'center'})
-    }, 0);
+      .add({
+        targets: '.easing-visualizer .bar',
+        scaleY: anime.stagger([1, 44], { easing: ease, from: 'center', direction: 'reverse' }),
+        delay: anime.stagger(7, { from: 'center' })
+      })
+      .add({
+        targets: '.easing-visualizer .dot',
+        translateY: anime.stagger(['-160px', '160px'], { easing: ease, from: 'last' }),
+        delay: anime.stagger(7, { from: 'center' })
+      }, 0);
 
   }
-  
+
   play();
-  
+
 })();
